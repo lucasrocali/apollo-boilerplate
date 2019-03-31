487fa62d01824a3bc7dafd5fa4d6e7727ce07736
Lucas Rocali <rocali@outlook.com>
2019-03-30 21:54:29 -0300

    feat: app module

---
+++ app/app/index.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React from 'react';
+import RootNavigation from './navigation';
+
+export default function App() {
+    return (
+        <RootNavigation />
+    );
+}
